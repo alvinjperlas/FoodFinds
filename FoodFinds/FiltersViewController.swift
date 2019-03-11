a173cc51da59dc5a5b6af81c54f943fab611494c
alvin <alvinjericho@gmail.com>
2019-03-10 19:18:52 -0700

    Made major changes to files and structure.
    Made major changes to files and structure.

--- FoodFinds/FiltersViewController.swift
+++ FoodFinds/FiltersViewController.swift
@@ -1,50 +1,50 @@
 //
-//  Created by Alvin Perlas on 2/15/19.
-//  Copyright © 2019 alvinperlas. All rights reserved.
+//  FiltersViewController.swift
+//  FoodFinds
+//
+//  Created by Alvin Perlas on 3/10/19.
+//  Copyright © 2019 Ken Toh. All rights reserved.
 //
 
+import Foundation
 import UIKit
+
+
 
 
 
 
 //Write the protocol declaration here:
 protocol FiltersDelegate {
-    func userEnteredFilter(data: YelpSearchFilter)
+  func userEnteredFilter(data: YelpSearchFilter)
 }
 
 
 class FiltersViewController: UIViewController {
+  
+  var delegate : FiltersDelegate?
+  @IBOutlet weak var categoryTextField: UITextField!
+  @IBOutlet weak var ratingControl: UISegmentedControl!
+  @IBOutlet weak var priceControl: UISegmentedControl!
+  @IBOutlet weak var isOpen: UISwitch!
+  
+  @IBAction func startExploring(_ sender: Any) {
+    let category = categoryTextField.text
+    let price = String(priceControl.selectedSegmentIndex)
+    let rating = ratingControl.selectedSegmentIndex
+    let open = isOpen.isOn
     
-    var delegate : FiltersDelegate?
-    @IBOutlet weak var categoryTextField: UITextField!
-    @IBOutlet weak var ratingControl: UISegmentedControl!
-    @IBOutlet weak var priceControl: UISegmentedControl!
-    @IBOutlet weak var isOpen: UISwitch!
+    let searchFilter = YelpSearchFilter(category: category!, rating: rating,price: price,open: open)
     
-    @IBAction func startExploring(_ sender: Any) {
-        let category = categoryTextField.text
-        let price = String(priceControl.selectedSegmentIndex)
-        let rating = ratingControl.selectedSegmentIndex
-        let open = isOpen.isOn
-        
-        let searchFilter = YelpSearchFilter(category: category!, rating: rating,price: price,open: open)
+    delegate?.userEnteredFilter(data: searchFilter)
     
-        delegate?.userEnteredFilter(data: searchFilter)
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    
-    
-    @IBAction func backButtonPressed(_ sender: AnyObject) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    
-    
-    
-    
-    
-    
-    
+    _ = navigationController?.popViewController(animated: true)
+  }
+  
+  
+  
+
+
 }
+
+
---
+++ FoodFinds/SuggestionsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SuggestionsViewController.swift
+//  FoodFinds
+//
+//  Created by Alvin Perlas on 3/10/19.
+//  Copyright © 2019 Ken Toh. All rights reserved.
+//
+
+import Foundation
